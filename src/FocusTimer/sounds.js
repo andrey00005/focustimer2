9d554f9aa24b18bce144ba6024250ada343de18b
Andrey Pereira <72459986+andrey00005@users.noreply.github.com>
2024-09-01 03:00:50 -0300

    Update sounds.js

--- src/FocusTimer/sounds.js
+++ src/FocusTimer/sounds.js
@@ -2,9 +2,9 @@ export const buttonPressAudio = new Audio('./assets/button-press.wav')
 export const kitchenTimer = new Audio('./assets/kichen-timer.mp3')
 export const bgAudio = new Audio('./assets/bg-audio.mp3')
 
-export const chuva = new Audio('./assets/audio/Chuva.wav')
-export const cafeteria = new Audio('./assets/audio/Cafeteria.wav')
-export const floresta = new Audio('./assets/audio/Floresta.wav')
-export const lareira = new Audio('./assets/audio/Lareira.wav')
+export const chuva = new Audio('./assets/audio/Chuva.mp3')
+export const cafeteria = new Audio('./assets/audio/Cafeteria.mp3')
+export const floresta = new Audio('./assets/audio/Floresta.mp3')
+export const lareira = new Audio('./assets/audio/Lareira.mp3')
 
 bgAudio.loop = true
